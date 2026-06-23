a205af415e899ecb40147f3da5799f382aee840e
Satyam Yadav <satyampiyush2007@gmail.com>
2026-06-23 23:32:12 +0530

    Reverted to gemini-1.5-flash to fix API limit 20 error

--- ai_engine.py
+++ ai_engine.py
@@ -16,7 +16,7 @@ from models import QuestionModel
 GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 genai.configure(api_key=GEMINI_API_KEY)
 
-gemini_model = genai.GenerativeModel("gemini-2.5-flash")
+gemini_model = genai.GenerativeModel("gemini-1.5-flash")
 
 # ==========================================
 # 🧠 VECTOR DATABASE LOADER
